2e082ba7d86d37ee1f0c4dcaae3f5536df26989f
Oleg833 <oleg.kovtun447@gmail.com>
2023-02-16 16:43:49 +0200

    gallery 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,26 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join('');
+galleryContainer.innerHTML = galleryMarkup;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  animationSpeed: 250,
+});
+console.log(SimpleLightbox);
